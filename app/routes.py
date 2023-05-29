a307a36a3d8ebf8af22f727efbad5151b5fe3ffb
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-05-29 14:40:04 +1200

    upload

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
-from app import app,db
+from app import app,db,os,secure_filename
 from app.models import Noodle
-from app.forms import MyForm, ItemEntry
+from app.forms import MyForm, ItemEntry, UploadFileForm 
 from flask import render_template
 
 @app.route('/',methods=["GET", "POST"])
 def home():
     items = Noodle.query.all()
     print(items)
-    return render_template("index.html",items=items)    
+    return render_template("index.html",items=items)
+
+@app.route('/upload', methods=['GET',"POST"])
+def upload():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        #file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) Issue here
+        return "Image has been uploaded"
+    return render_template('upload.html', form=form)
